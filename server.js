9bfdd2c1d8dc29649812bffeec836288ee944fd6
satimbaevmuhammad <satimbaevmuhammad3@gmail.com>
2025-07-14 19:51:59 +0500

    done

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const app = require('./src/app');
+const app = require('./app');
 const connectDB = require('./src/config/db');
 
 const PORT = process.env.PORT || 5000;
