5280a46e05eaf31c5e1267a28bd913817ded1f59
satimbaevmuhammad <satimbaevmuhammad3@gmail.com>
2025-07-15 23:51:53 +0500

    done

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDoc = require('./src/config/swagger');
+const swaggerSpec = require('./src/config/swagger');
 const userRoutes = require('./src/routes/user.routes');
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
 
-// Routes
 app.use('/api/users', userRoutes);
 
-// Swagger
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+// ✅ Swagger
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// 404
 app.use((req, res) => res.status(404).json({ message: 'Route not found' }));
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-const express = require('express');
 const dotenv = require('dotenv');
-const userRoutes = require('./src/routes/user.routes');
-
 dotenv.config();
-const app = express();
 
-app.use(express.json());
-
-app.use('/api/users', userRoutes);
+const app = require('./app'); // app.js fayldan `app` ni import qilamiz
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
