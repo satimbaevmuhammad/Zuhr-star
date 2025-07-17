dbd4ffb0559c57bba5a31e6041415a26389e8b62
zerorich <167667718+zerorich@users.noreply.github.com>
2025-07-17 18:30:52 +0500

    Update swagger.js

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -17,7 +17,7 @@ const options = {
     components: {
       securitySchemes: {
         bearerAuth: {
-          type: 'https',
+          type: 'http',
           scheme: 'bearer',
           bearerFormat: 'JWT',
         }
