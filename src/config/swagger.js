a58b2829a8a49cbab25da4120780a0d19f0e3274
zerorich <167667718+zerorich@users.noreply.github.com>
2025-07-18 16:35:43 +0500

    Update swagger.js

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:5000/',
+        url: 'https://zuhrstar-production.up.railway.app/',
       },
     ],
     components: {
